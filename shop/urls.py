707c605a05acf042276971dbcaf1b3d49e03fd67
ruch.nastya@gmail.com <ruch.nastya@gmail.com>
2023-02-20 10:11:17 +0300

    add buttuns

--- shop/urls.py
+++ shop/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     # path('order/<int:id>/', views.GetProduct, name='product_url'),
     path('product/<int:id>/', views.GetProd, name='prod_url'),
     path('sendText', views.sendText, name='sendText'),
-    # path('product/1/', views.GetPr1, name='product1'),
+    # path('da', views.da, name='da'),    # path('product/1/', views.GetPr1, name='product1'),
     # path('product/2/', views.GetPr2, name='product2'),
     # path('product/3/', views.GetPr3, name='product3'),
 ]
